6b338883c20e042489a713a319ed6378559a25b5
briankipchirchir <kbrianngetich@gmail.com>
2024-07-21 18:30:46 +0300

    kk

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
+
 function addingEventListener() {
+  const button = document.getElementById('button');
+  button.addEventListener('click', () => {
+    // Code to handle the click event
+    console.log('Button clicked');
+  });
 }
+addingEventListener();
